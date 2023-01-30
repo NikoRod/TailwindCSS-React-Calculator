8ba6d277d51dbe24f7945e6fa688d188c15215c6
NikoRod <94146334+NikoRod@users.noreply.github.com>
2023-01-30 15:28:20 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+      <button className='bg-blue-500 text-lime-300'>Hola</button>
+      <h3>Adios</h3>
+
+    </div>
+  );
+}
+
+export default App;
